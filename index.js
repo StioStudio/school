d5eab1fd40cb98743efb6b09e550364e0da0f450
stio <dev@stio.studio>
2023-10-27 17:43:33 +0200

    First update

---
+++ index.js
@@ -0,0 +1,9 @@
+// let rem = await(await fetch("/assets/projects.json")).json()
+let rem = await(await fetch("./projects.json")).json()
+contentCreate(rem)
+// i18n.getTra().forEach(e => {
+//     console.log(e)
+// });
+console.log(i18n.createMessagesJSON())
+await i18n.setLanguage("no")
+i18n.tranlatePage()
